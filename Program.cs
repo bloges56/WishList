2a937b5a5814d39ef070a5f9bdd4b0dad1c91cb5
bloges56 <brady.drew.logan@gmail.com>
2020-11-16 13:21:07 -0600

    displays wishes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WishList
 {
@@ -6,7 +7,21 @@ namespace WishList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             List<string> wishes = new List<string>()
+            {
+                "An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'",
+                "The ability to think of interesting wishes on short notice",
+                "A washtub of Skittles",
+                "World peace, but not the kind of world peace where all the humans are removed from existence",
+                "A TV that is NOT internet-capable",
+                "The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"
+            };
+            Console.WriteLine("My wishes...");
+
+            foreach(string wish in wishes)
+            {
+                Console.WriteLine(wish);
+            }
         }
     }
 }
